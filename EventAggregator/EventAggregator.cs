888ddd95dfbeceaaf98db0d67af311fe81ebd8c0
shahboura <shehabelhadidy@hotmail.com>
2017-02-12 21:48:29 +0200

    - Implementation of EventAggregator

--- EventAggregator/EventAggregator.cs
+++ EventAggregator/EventAggregator.cs
@@ -1,32 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EventAggregator
 {
     public class EventAggregator
     {
+        private readonly object _lockObject = new object();
+
+        private readonly Dictionary<Type, List<Subscriber>> _subscribers =
+            new Dictionary<Type, List<Subscriber>>();
+
         public void Publish<T>(T data)
         {
-            
+            var type = typeof (T);
+            if (!_subscribers.ContainsKey(type))
+            {
+                return;
+            }
+
+            var subscribers = _subscribers[type];
+            lock (_lockObject)
+            {
+                subscribers.RemoveAll(s => !s.Context.IsAlive);
+            }
+
+            subscribers.ForEach(s => ((Action<T>) s.Action)(data));
         }
 
         public void Subscribe<T>(object context, Action<T> action)
         {
-            
+            var type = typeof (T);
+            var subscriber = new Subscriber
+            {
+                Context = new WeakReference(context),
+                Action = action
+            };
+
+            lock (_lockObject)
+            {
+                if (_subscribers.ContainsKey(type))
+                {
+                    _subscribers[type].Add(subscriber);
+                }
+                else
+                {
+                    _subscribers[type] = new List<Subscriber> {subscriber};
+                }
+            }
         }
 
         public void Unsubscribe(object context)
         {
-            
+            lock (_lockObject)
+            {
+                foreach (var subscribers in _subscribers.Values)
+                {
+                    subscribers.RemoveAll(
+                        s => !s.Context.IsAlive || s.Context.Target == context);
+                }
+            }
         }
 
         public void Unsubscribe<T>()
         {
-            
+            _subscribers.Remove(typeof (T));
         }
 
         public void Unsubscribe<T>(object context)
         {
-            
+            List<Subscriber> subscribers;
+
+            if (!_subscribers.TryGetValue(typeof (T), out subscribers))
+            {
+                return;
+            }
+
+            lock (_lockObject)
+            {
+                subscribers.RemoveAll(
+                    s => !s.Context.IsAlive || s.Context.Target == context);
+            }
         }
     }
 }
---
+++ EventAggregator/Subscriber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EventAggregator
+{
+    internal class Subscriber
+    {
+        public WeakReference Context { get; set; }
+
+        public Delegate Action { get; set; }
+    }
+}
